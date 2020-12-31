27326d069187e5577b184a2f202eb062fe6e1bf8
JeesooHa <jsha2913@gmail.com>
2020-12-31 21:17:15 +0900

    [이것이 C# 이다] 코드 추가

---
+++ thisiscsharp/ch3/IntegerLiterals/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerLiterals
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            byte a = 240;
+            Console.WriteLine($"a={a}");
+
+            byte b = 0b1111_0000;   
+            Console.WriteLine($"b={b}");
+
+
+
+        }
+    }
+}
